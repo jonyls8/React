8bd0ebfde3e33f2c4a0716570f495a846c48fb08
jony <xjafnerx@gmail.com>
2020-07-15 15:10:57 +0200

    Updates

---
+++ precache-manifest.a8895576ff0859a68ef3f9951187eb4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66297818b649f8cba6e6760f8d9fd1af",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "249df912a414797afe59",
+    "url": "/React/static/css/main.3441dea7.chunk.css"
+  },
+  {
+    "revision": "68e09645afff03b820eb",
+    "url": "/React/static/js/2.ede15c37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.ede15c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249df912a414797afe59",
+    "url": "/React/static/js/main.3cb3ba06.chunk.js"
+  },
+  {
+    "revision": "556fe4f80fc31acf5196",
+    "url": "/React/static/js/runtime-main.d7d2359f.js"
+  }
+]);
